08ed5cd6ff31ec88d0eac119f637b2b3ce6f403d
britofelipe <arrudabritofelipe@gmail.com>
2023-03-05 22:24:09 -0300

    Routes: first setup

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@home');
+
+Route::get('/about', 'AboutController@about');
+
+Route::get('/contact', 'ContactController@contact');
