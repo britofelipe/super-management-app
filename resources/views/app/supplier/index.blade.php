136fb894adca63bde7d9502708d52132bbaf33ef
britofelipe <arrudabritofelipe@gmail.com>
2023-03-08 16:42:18 -0300

    Blade: Loop variable

--- resources/views/app/supplier/index.blade.php
+++ resources/views/app/supplier/index.blade.php
@@ -8,15 +8,25 @@
 
 @isset($suppliers)
     @forelse ($suppliers as $index => $supplier)
+        Current iteration: {{ $loop->iteration}}
+        <br>
         {{-- Doesn't affect original array --}}
-        Supplier: @{{ $supplier['name'] }}
+        Supplier: {{ $supplier['name'] }}
         <br>
-        Status: @{{ $supplier['status'] }}
+        Status: {{ $supplier['status'] }}
         <br>
-        CNPJ: @{{$supplier['cnpj'] ?? ''}}
+        CNPJ: {{$supplier['cnpj'] ?? ''}}
         <br>
-        Telephone: @({{ $supplier['ddd'] ?? ''}}) {{ $supplier['telephone'] ?? ''}}
+        Telephone:  ({{ $supplier['ddd'] ?? ''}}) {{ $supplier['telephone'] ?? ''}}
+        <br>
+        @if($loop->first)
+            First iteration of loop
+        @endif
+        @if($loop->last)
+            Last iteration of loop
+        @endif
         <hr>
+        {{-- The @dd() command helps to know information about the loop --}}
     @empty
         There aren't any suppliers registered
     @endforelse
