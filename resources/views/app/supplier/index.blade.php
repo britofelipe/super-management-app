c48c6b9a0b17fad0bc2118a275a64a574bb3e497
britofelipe <arrudabritofelipe@gmail.com>
2023-03-08 15:30:09 -0300

    Blade: For

--- resources/views/app/supplier/index.blade.php
+++ resources/views/app/supplier/index.blade.php
@@ -2,37 +2,21 @@
 
 @php
     /*
-    if(empty($variable)) // true if the variable is empty
-        - ''
-        - 0
-        - 0.0
-        - '0'
-        - null
-        - false
-        - array()   -> empty array
-        - $var      -> no value attributed
+
     */
 @endphp
 
 @isset($suppliers)
-    Supplier: {{ $suppliers[0]['name'] }}
-    <br>
-    Status: {{ $suppliers[0]['status'] }}
-    <br>
-    CNPJ: {{$suppliers[1]['cnpj'] ?? ''}}
-    <br>
-    Telephone: ({{ $suppliers[1]['ddd'] ?? ''}}) {{ $suppliers[1]['telephone'] ?? ''}} -
-    @switch($suppliers[1]['ddd'])
-        @case('11')
-            São Paulo - SP
-            @break
-        @case('32')
-            Juiz de Fora - MG
-            @break
-        @case('85')
-            Fortaleza - CE
-            @break
-        @default
-            State not found
-    @endswitch
+    @for ($i = 0; isset($suppliers[$i]); $i++)
+        Supplier: {{ $suppliers[$i]['name'] }}
+        <br>
+        Status: {{ $suppliers[$i]['status'] }}
+        <br>
+        CNPJ: {{$suppliers[$i]['cnpj'] ?? ''}}
+        <br>
+        Telephone: ({{ $suppliers[$i]['ddd'] ?? ''}}) {{ $suppliers[$i]['telephone'] ?? ''}}
+        <hr>
+    @endfor
 @endisset
+
+
